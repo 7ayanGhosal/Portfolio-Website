1c6e90c881dcc6108ae95a1947a66699e93fdd31
Ayan Ghosal <ayan.ghosal007@gmail.com>
2020-07-15 11:41:33 +0530

    Starting page preloader done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Layout from './Containers/Layout/Layout'
-import Preloader from './Components/Preloader/Preloader';
 
 class App extends Component {
-
-  // state = {
-  //   preloader: <Preloader/>
-  // }
-
-  // preloader = ()=>{
-  //   console.log("Loaded!!");
-  //   setTimeout(()=>{
-  //     this.setState({preloader: <Layout/>})
-  //   }, 2000)
-  // }
   
   render() {
     return (
       <div className={classes.App}>
-        {/* {this.state.preloader} */}
         <Layout/>
       </div>
     );

15acd608c8c458e162a7032a4f4eb0c02bbd330a
7ayanGhosal <ayan.ghosal007@gmail.com>
2021-04-13 18:55:28 +0530

    updated profile pic size

--- src/Components/ProfilePic/ProfilePic.js
+++ src/Components/ProfilePic/ProfilePic.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
-import classes from './ProfilePic.css';
-import image from '../../Assets/ProfilePic/img.jpg';
+import React, { Component } from "react";
+import classes from "./ProfilePic.css";
+import image from "../../Assets/ProfilePic/img.jpg";
 
-class ProfilePic extends Component{
-    render(){
-        return(
-            <div className={classes.ProfilePic} >
-                <div>
-                    <img src = {image} alt="My Image" />
-                </div>
-            </div>
-        )
-    }
+class ProfilePic extends Component {
+  render() {
+    return (
+      <div className={classes.ProfilePic}>
+        <div>
+          <img src={image} alt="My Image" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProfilePic;
